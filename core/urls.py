1397d23b6081524f2963514dd1b56707d0b3d9ff
unknown <mr.mhdi1995@gmail.com>
2022-11-15 13:01:11 +0330

    added authentication with JWT

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (
+    CostumeTokenObtainPairView, RegisterView, ChangePasswordView,
 )
 
+
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CostumeTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/registeration/', RegisterView.as_view(), name='auth_register'),
+    path('api/password/change/<int:pk>', ChangePasswordView.as_view(), name='auth_change_password'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.generics import (
+    CreateAPIView, UpdateAPIView,
+)
+from django.contrib.auth import get_user_model
 
+from .serializers import (
+    CostumeTokenObtainPairSerializer, RegisterSerializer,ChangePasswordSerializer
+)
 # Create your views here.
+
+User = get_user_model()
+
+class CostumeTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CostumeTokenObtainPairSerializer
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
