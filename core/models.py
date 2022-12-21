b928dae6b9023550a6d02b7081f2382a6e2a06bf
unknown <mr.mhdi1995@gmail.com>
2022-12-21 19:40:13 +0330

    default of is_active is now False

--- core/models.py
+++ core/models.py
@@ -6,4 +6,15 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
-    image = models.ImageField(upload_to='media/images/users/', default='media/images/users/default.png')
+    image = models.ImageField(
+        upload_to='media/images/users/', 
+        default='media/images/users/default.png'
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
