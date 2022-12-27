96285bc1f0968ba53858cd6e1c9a428494e82f88
unknown <mr.mhdi1995@gmail.com>
2022-12-27 12:29:35 +0330

    added verbose_name to models and fields

--- bookstack/models.py
+++ bookstack/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.template.defaultfilters import slugify
 from django.core.files import File
+from django.utils.html import format_html
 from taggit.managers import TaggableManager
 from PIL import Image
 from io import BytesIO
@@ -12,36 +13,40 @@ User = get_user_model()
 
 def _compress_image(image):
     img = Image.open(image)
-    img.convert('RGB')
     img_io = BytesIO() 
-    img.save(img_io, 'JPEG', quality=70, optimize=True) 
+    if img.mode == 'RGBA':
+        img.save(img_io, 'PNG', quality=70, optimize=True) 
+    else:
+        img.save(img_io, 'JPEG', quality=70, optimize=True) 
     new_image = File(img_io, name=image.name)
     return new_image
 
 def _create_thumbnail(image, size=(240, 240)):
     img = Image.open(image)
-    img.convert('RGB')
-    img.thumbnail(size)
-    thumb_io = BytesIO()
-    img.save(thumb_io, 'JPEG', quality=40)
+    thumb_io = BytesIO() 
+    if img.mode == 'RGBA':
+        img.save(thumb_io, 'PNG', quality=40) 
+    else:
+        img.save(thumb_io, 'JPEG', quality=40) 
     thumbnail = File(thumb_io, name=image.name)
     return thumbnail
 
 class Shelve(models.Model):
-    creature = models.ForeignKey(User , on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=250, blank=True)
+    creature = models.ForeignKey(User , on_delete=models.SET_NULL, null=True, verbose_name='ایجاد کننده')
+    name = models.CharField(max_length=50, verbose_name='نام قفسه')
+    description = models.CharField(max_length=250, blank=True, verbose_name='توضیحات')
     slug = models.SlugField(unique=True,max_length=50, blank=True)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    image = models.ImageField(upload_to='images/shelves/', default='images/default_shelve.jpg')
+    is_active = models.BooleanField(default=True, verbose_name='وضعیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='زمان انتشار')
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/shelves/', default='images/default_shelve.jpg', verbose_name='عکس')
     thumbnail = models.ImageField(upload_to='images/shelve/thumbnail/', default='images/default_shelve.jpg')
-    tags = TaggableManager()
-
+    tags = TaggableManager(verbose_name='بر چسب ها')
 
     class Meta:
         ordering = ('-name',)
+        verbose_name = 'قفسه'
+        verbose_name_plural = 'قفسه ها'
     
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -53,25 +58,31 @@ class Shelve(models.Model):
     
     def __str__(self) -> str:
         return self.name
+    
+    def thumbnail_tag(self):
+        return format_html(f'<img width=125 hiegth=100 style="border-radius: 10px" src="{self.thumbnail.url}" />')
+    thumbnail_tag.short_description = 'عکس'
 
 
 class Book(models.Model):
-    shelve = models.ForeignKey(Shelve, on_delete=models.SET_NULL, null=True, related_name='books')
-    creature = models.ForeignKey(User , on_delete=models.SET_NULL, null=True)
-    authors = models.ManyToManyField(User, related_name='authors')
-    name = models.CharField(max_length=25)
-    publish = models.BooleanField(default=False)
-    description = models.CharField(max_length=250)
-    created_at = models.DateTimeField(auto_now_add=True)
+    shelve = models.ForeignKey(Shelve, on_delete=models.SET_NULL, null=True, related_name='books', verbose_name='قفسه')
+    creature = models.ForeignKey(User , on_delete=models.SET_NULL, null=True, verbose_name='ایجاد کننده')
+    authors = models.ManyToManyField(User, related_name='authors', verbose_name='نویسندگان')
+    name = models.CharField(max_length=25, verbose_name='نام')
+    published = models.BooleanField(default=False, verbose_name='وضعیت')
+    description = models.CharField(max_length=250, verbose_name='توضیحات')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='زمان انتشار')
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True, blank=True)
-    image = models.ImageField(upload_to='images/books/', default='images/default_book.jpg')
+    image = models.ImageField(upload_to='images/books/', default='images/default_book.jpg', verbose_name='عکس')
     thumbnail = models.ImageField(upload_to='images/books/thumbnail/', default='images/default_book.jpg')
-    tags = TaggableManager()
+    tags = TaggableManager(verbose_name='برچسب ها')
     views_count = models.PositiveIntegerField(default=0)
     
     class Meta:
         ordering = ('-name',)
+        verbose_name = 'کتاب'
+        verbose_name_plural = 'کتاب ها'
     
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -83,6 +94,11 @@ class Book(models.Model):
     
     def __str__(self) -> str:
         return self.name
+        
+    def thumbnail_tag(self):
+        return format_html(f'<img width=125 hiegth=100 style="border-radius: 10px" src="{self.thumbnail.url}" />')
+    thumbnail_tag.short_description = 'عکس'
+
 
 
 class BookViews(models.Model):
@@ -94,25 +110,27 @@ class BookViews(models.Model):
 
 
 class Chapter(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='chapter')
-    name = models.CharField(max_length=70)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='chapter', verbose_name='کتاب')
+    name = models.CharField(max_length=70, verbose_name='نام')
+    description = models.CharField(max_length=255, blank=True, null=True, verbose_name='توضیحات')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='زمان انتشار')
     
     class Meta:
         ordering = ('-created_at',)
+        verbose_name = 'فصل'
+        verbose_name_plural = 'فصول'
     
     def __str__(self) -> str:
         return self.name
 
 class Page(models.Model):
-    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name='page')
-    name = models.CharField(max_length=150, null=True) # in deployment change null to False
-    text = models.TextField()
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name='page', verbose_name='قفسه')
+    name = models.CharField(max_length=150, null=True, verbose_name='نام') # in deployment change null to False
+    text = models.TextField(verbose_name='متن')
     slug = models.SlugField(unique=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    tags = TaggableManager()
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='زمان انتشار')
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = TaggableManager(verbose_name='برچسب ها')
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -121,11 +139,13 @@ class Page(models.Model):
     
     class Meta:
         ordering = ('id',)
+        verbose_name = 'صفحه'
+        verbose_name_plural = 'صفحات'
 
 
 class PageImage(models.Model):
-    image = models.ImageField(upload_to='images/pages/')
-    page = models.ManyToManyField(Page)
+    image = models.ImageField(upload_to='images/pages/', verbose_name='عکس')
+    page = models.ManyToManyField(Page, verbose_name='صفحه')
     
     def save(self, *args, **kwargs):
         self.image = _compress_image(self.image)
@@ -133,10 +153,13 @@ class PageImage(models.Model):
 
 
 class PageReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='reviews', default=None) # remove the default in deployment
-    created_at = models.DateField(auto_now_add=True)
-    text = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='reviews', default=None, verbose_name='صفحه') # remove the default in deployment
+    created_at = models.DateField(auto_now_add=True, verbose_name='زمان انتشار')
+    text = models.CharField(max_length=255, verbose_name='متن')
+    
+    def __str__(self) -> str:
+        return self.user.username
 
 
 class Activity(models.Model):
@@ -151,15 +174,17 @@ class Activity(models.Model):
         BOOK = 'book'
         PAGE = 'page'
     
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    activity = models.CharField(choices=ActivityType.choices, max_length=1)
-    model_type = models.CharField(choices=ModelType.choices, max_length=6)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
-    shelve = models.ForeignKey(Shelve, on_delete=models.CASCADE, null=True, blank=True)
-    page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    activity = models.CharField(choices=ActivityType.choices, max_length=1, verbose_name='نوع فعالیت')
+    model_type = models.CharField(choices=ModelType.choices, max_length=6, verbose_name='نوع مدل')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True, verbose_name='کتاب')
+    shelve = models.ForeignKey(Shelve, on_delete=models.CASCADE, null=True, blank=True, verbose_name='قفسه')
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True, blank=True, verbose_name='صفحه')
+    name = models.CharField(max_length=150, verbose_name='نام')
     slug = models.SlugField(blank=False)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True, verbose_name='زمان انتشار')
     
     class Meta:
         ordering = ('-created_at',)
+        verbose_name = 'فعالیت'
+        verbose_name_plural = 'فعالیت ها'
