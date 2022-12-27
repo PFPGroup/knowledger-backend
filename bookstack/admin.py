4db122923383e6d157c41423d4cf43cb73a9a469
unknown <mr.mhdi1995@gmail.com>
2022-12-27 12:35:29 +0330

    create new admin area just for bookstack

--- bookstack/admin.py
+++ bookstack/admin.py
@@ -5,9 +5,49 @@ from .models import (
 )
 # Register your models here.
 
-admin.site.register(Shelve)
-admin.site.register(Book)
-admin.site.register(Chapter)
+admin.site.index_title = 'مدیریت سایت'
+admin.site.site_header = 'ادمین کتابخانه'
+admin.site.site_title = 'مدیریت سایت'
+
+
+class BookStackAdminَArea(admin.AdminSite):
+    site_header = 'مدیریت کتابخانه'
+bookstack_admin = BookStackAdminَArea(name='BookstackAminArea')
+
+
+class ShelveAdminModel(admin.ModelAdmin):
+    list_display = ('thumbnail_tag', 'name', 'created_at', 'is_active')
+    list_filter = ('is_active', 'updated_at')
+    search_fields = ('creature', 'name', 'description', 'tags')
+    exclude = ('slug', 'thumbnail')
+
+
+class BookAdminModel(admin.ModelAdmin):
+    list_display = ('thumbnail_tag', 'name', 'creature', 'published')
+    list_filter = ('published', 'created_at', 'updated_at')
+    search_fields = ('creature', 'name', 'description', 'tags')
+    exclude = ('slug', 'thumbnail')
+
+
+class ChapterAdminModel(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'description')
+    exclude = ('slug', 'thumbnail')
+
+
+class PageAdminModel(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'description', 'tags')
+    exclude = ('slug', 'thumbnail')
+
+
+bookstack_admin.register(Shelve, ShelveAdminModel)
+bookstack_admin.register(Book, BookAdminModel)
+bookstack_admin.register(Chapter, ChapterAdminModel)
+bookstack_admin.register(Page, PageAdminModel)
+
 admin.site.register(Page)
 admin.site.register(PageReview)
 admin.site.register(Activity)
