d461c48a6145f2114c4f76cae1d58d73ebe37259
unknown <mr.mhdi1995@gmail.com>
2022-12-28 23:57:18 +0330

    def ready for use signals

--- bookstack/apps.py
+++ bookstack/apps.py
@@ -5,3 +5,6 @@ class BookstackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookstack'
     verbose_name = 'کتابخانه'
+    
+    def ready(self) -> None:
+        import bookstack.signals
